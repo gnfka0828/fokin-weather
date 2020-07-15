2429c83c682baf518dd093258c95d51cd1e7310c
yeon.hoo <gnfka0828@gmail.com>
2020-07-15 12:07:35 +0900

    feat : Getting the Location and the Weather
    1. Getting the Location
    2. Asking the Permissions
    3. Getting the Weather

--- App.js
+++ App.js
@@ -1,6 +1,47 @@
 import React from 'react';
+import { Alert } from "react-native";
 import Loading from "./Loading";
+import * as Location from "expo-location";
+import axios from "axios";
 
-export default function App() {
-  return <Loading />;
+const API_KEY = "6167954107d055a5c2ae5346ed03458e";
+
+export default class extends React.Component {
+  // React.Component 뒤에 () 붙이지 말자.
+
+  state = {
+    isLoading: true
+  }
+
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
+    console.log(data);
+  };
+
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();
+
+      const { 
+        coords: { latitude, longitude } 
+      } = await Location.getCurrentPositionAsync();
+
+      this.getWeather(latitude, longitude);
+      this.setState({ isLoading: false });
+
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
+    }
+  }
+  
+  componentDidMount() {
+    this.getLocation();
+  }
+
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  };
 }
