b51a19229c161914ed3c40e479e322c6e19f4244
yeon.hoo <gnfka0828@gmail.com>
2020-07-15 10:39:54 +0900

    feat : Add Loading Screen
    feat : Add Loading Screen

---
+++ Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Loading() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Getting the fucking weather</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "flex-end",
+        paddingHorizontal: 30,
+        paddingVertical: 100,
+        backgroundColor: "#FDF6AA"
+    },
+    text: {
+        color: "#2c2c2c",
+        fontSize: 30 /* 30px, 30px이라고 직접 쓰려면 "30px" 처럼 써야 함. */
+    }
+});
